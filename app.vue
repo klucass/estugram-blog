0fe8cf6e80f2e960503ba4829b69d6b0ad872b21
Kennedy <kennedy.lucas@gmail.com>
2024-11-03 14:36:02 -0300

    Configura titulo do site

--- app.vue
+++ app.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+useHead({
+  title: "Estugram Blog",
+  meta: [{ name: "description", content: "Dicas de estudo e produtividade" }],
+});
+</script>
 <template>
   <main>
     <NuxtPage />
